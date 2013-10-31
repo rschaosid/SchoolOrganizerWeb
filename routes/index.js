06b029096950d6b748af07ed15fc29b8a67e525f
rschaosid <rschaosid@gmail.com>
2013-10-31 15:45:06 -0500

    several things 'cause I'm too lazy to do itsy bitsy commits

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,22 @@ var Student = java.import("com.jakobcornell.compsci.schoolorganizer.Student"),
     StudentBody = java.import("com.jakobcornell.compsci.schoolorganizer.StudentBody"),
     Grade = java.import("com.jakobcornell.compsci.schoolorganizer.Grade");
 
+var startTime = new Date(2013, 1, 1);
+var endTime = new Date(2014, 1, 1);
+
 
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
-  var student = new Student("Jakob Cornell");
-  res.render("index", {studentName: student.toStringSync()});
+  res.render("index");
+};
+
+var classes = [];
+
+classes.push(new Class(startTime, endTime));
+
+exports.classList = function(req, res) {
+  res.render("classList", {classes: classes});
 };

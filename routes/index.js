f51ce76c3695e75a239bd776a7e7e35634dd4726
rschaosid <rschaosid@gmail.com>
2013-10-31 15:25:47 -0500

    very cool beans bro

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
+var java = require('java');
+
+java.classpath.push("lib/SchoolOrganizer.jar");
+var Student = java.import("com.jakobcornell.compsci.schoolorganizer.Student"),
+    Class = java.import("com.jakobcornell.compsci.schoolorganizer.Class"),
+    StudentBody = java.import("com.jakobcornell.compsci.schoolorganizer.StudentBody"),
+    Grade = java.import("com.jakobcornell.compsci.schoolorganizer.Grade");
+
 
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  var student = new Student("Jakob Cornell");
+  res.render("index", {studentName: student.toStringSync()});
 };
